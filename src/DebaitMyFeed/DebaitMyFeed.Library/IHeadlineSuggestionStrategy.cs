2225e080d1bd9eab09b89c17117c38ecf67d6d8f
Stig Voss <stigvoss@noreply.github.com>
2025-02-09 13:03:48 +0100

    chore: clean-up obsolete suggestion method

--- src/DebaitMyFeed/DebaitMyFeed.Library/IHeadlineSuggestionStrategy.cs
+++ src/DebaitMyFeed/DebaitMyFeed.Library/IHeadlineSuggestionStrategy.cs
@@ -2,8 +2,5 @@ namespace DebaitMyFeed.Library;
 
 public interface IHeadlineSuggestionStrategy
 {
-    [Obsolete("Use SuggestHeadlineAsync(Article) instead.")]
-    public Task<string?> SuggestHeadlineAsync(string articleText);
-    
     public Task<string?> SuggestHeadlineAsync(Article article);
 }
--- src/DebaitMyFeed/DebaitMyFeed.Library/OpenAiHeadlineSuggestionStrategy.cs
+++ src/DebaitMyFeed/DebaitMyFeed.Library/OpenAiHeadlineSuggestionStrategy.cs
@@ -23,38 +23,6 @@ public class OpenAiHeadlineSuggestionStrategy : IHeadlineSuggestionStrategy
         this.openAiClient = new OpenAIClient(openAiOptions.ApiKey);
         this.chatClient = this.openAiClient.GetChatClient(openAiOptions.Model);
     }
-    
-    public async Task<string?> SuggestHeadlineAsync(string articleText)
-    {
-        string prompt = 
-            """
-                The user will provide you the text of the article.
-                You will use the text to generate a clear, concise, specific 
-                and descriptive headline free of clickbait for the article.
-                Focus on key points of the article.
-                Keep a neutral tone. 
-                If the key people in the article are expected to be known by the reader, consider including their name,
-                otherwise, if key people has a title significant to the article, consider including the title.
-                If the article mentions a location key to the article, consider including the location.
-                If the article mentions commonly known organizations, consider including the organization.
-                Keep the headline in the language of the article. 
-                You will only give the headline, nothing else.
-            """;
-        
-        var messages = new List<ChatMessage>
-        {
-            new SystemChatMessage(prompt),
-            new UserChatMessage(articleText)
-        };
-
-        ChatCompletionOptions options = new()
-        {
-            Temperature = 0.5f
-        };
-        ClientResult<ChatCompletion>? chatCompletion = await chatClient.CompleteChatAsync(messages, options);
-
-        return chatCompletion.Value.Content.FirstOrDefault()?.Text;
-    }
 
     public async Task<string?> SuggestHeadlineAsync(Article article)
     {
